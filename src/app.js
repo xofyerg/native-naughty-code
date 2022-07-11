09340090f27ad635d7b5403402c5859d44e647c4
xofyerg <xofyerg1@gmail.com>
2022-07-11 19:42:00 +0300

    some code

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 import "./styles/style.css";
+import { isValid } from "./helpers/validation";
 
-console.log("Working?...");
+const form = document.getElementById("input-form");
+const input = form.querySelector("#input");
+const submitBtn = form.querySelector("#submit");
+
+form.addEventListener("submit", submitFormHandler);
+
+function submitFormHandler(e) {
+  e.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    submitBtn.disabled = true;
+
+    input.value = "";
+    submitBtn.disabled = false;
+  }
+}
